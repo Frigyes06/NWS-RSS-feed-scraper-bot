54a680ec20718d2132a39f37218585f0a453e8ba
Frigyes06 <72750891+Frigyes06@users.noreply.github.com>
2022-06-05 12:38:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,19 +10,23 @@ The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 '''
 
+from multiprocessing.connection import Client
 import requests
 from bs4 import BeautifulSoup
 import discord
+from discord.ext import tasks
 
 #this is the url to Sonoma County's NWS RSS feed
 default_url = "https://alerts.weather.gov/cap/wwaatmget.php?x=CAC097"
 
 client = discord.Client()
 
-discord_token = "YOUR TOKEN"
+discord_token = "YOUR_TOKEN_HERE"
 
 current_watches = []
+previous_watches = []
 current_zone = ""
+bot_ok = False
 
 def getwarnings(url):
     global current_zone
@@ -53,12 +57,14 @@ def getwarnings(url):
         watch['expires'] = entry.expires.text
         current_watches.append(watch)
     return True
-        
+
+
 @client.event
 async def on_ready():
     game = discord.Game("!help")
     await client.change_presence(status=discord.Status.online, activity=game)
     print('We have logged in as {0.user}'.format(client))
+    bot_ok = True
 
 @client.event
 async def on_message(message):
@@ -82,6 +88,8 @@ async def on_message(message):
                 status = getwarnings(message.content.split(" ")[1])
             elif message.content.split(" ")[1].startswith("california") or message.content.split(" ")[1].startswith("California"):
                 status = getwarnings("https://alerts.weather.gov/cap/ca.php?x=1")
+            elif message.content.split(" ")[1].startswith("test"):
+                await message.channel.send("This is a test of the serious warning system. <@&983063004914520115> I repeat this is a test")
             else:
                 status = getwarnings("https://alerts.weather.gov/cap/wwaatmget.php?x=" + message.content.split(" ")[1])
         
@@ -94,5 +102,32 @@ async def on_message(message):
         
         await message.channel.send(current_zone)
         await message.channel.send("There are no active watches, warnings or advisories")
+
+@tasks.loop(minutes=1)
+async def get_new_warnings():
+    to_announce = []
+    print("started retrieving warnings")
+    getwarnings(default_url)
+    for watch in current_watches:
+        if watch in previous_watches:
+            pass
+        if watch not in previous_watches:
+            to_announce.append(watch)
+            previous_watches.append(watch)
+    for watch in previous_watches:
+        if watch in current_watches:
+            pass
+        if watch not in current_watches:
+            previous_watches.remove(watch)
+    print(to_announce)
+    if bot_ok:
+        channel = Client.get_channel('919341495243407380')
+        await channel.send("New Watches, Warnings and Advisories for Sonoma (CAC097) California Issued by the National Weather Service")
+        for watch in current_watches:
+            embed = discord.Embed(title = watch['title'], url = watch['link'], description = watch['summary'])
+            await channel.send(embed = embed)
+
+
+get_new_warnings.start()
 
 client.run(discord_token)
